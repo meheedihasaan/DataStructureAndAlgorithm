cc0272bb5a31629a741ee937fb150e1396805bdb
Mehedi Hasan <mehedihasan111941@gmail.com>
2023-03-30 04:55:06 -0700

    Add Pattern5.java

---
+++ src/main/java/pattern/Pattren5.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pattern;
+
+/**
+ *
+ * @author User
+ */
+
+//Problem Link: https://practice.geeksforgeeks.org/problems/triangle-pattern/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=pattern_5
+
+public class Pattren5 {
+    
+    public void printTriangle(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n-i; j++) {
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+    }
+    
+}
