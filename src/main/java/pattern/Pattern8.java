dd2b269ae76a8e5e02ae46cb2406733c04d65460
Mehedi Hasan <mehedihasan111941@gmail.com>
2023-03-30 09:42:07 -0700

    Add Pattern8.java

--- src/main/java/pattern/Pattern8.java
+++ src/main/java/pattern/Pattern8.java
@@ -9,28 +9,28 @@ package pattern;
  * @author User
  */
 
-//Problem Link: https://practice.geeksforgeeks.org/problems/triangle-pattern-1661492263/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=pattern_7
+//Problem Link: https://practice.geeksforgeeks.org/problems/triangle-pattern-1661493231/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=pattern_8
 
-public class Pattern7 {
+public class Pattern8 {
     
-    public void printTriangle(int n) {
-        for(int i = 0; i < n; i++){
+    public void printTriangle(int n){
+        for (int i = 0; i < n; i++) {            
             //Space
-            for(int j = 0; j < n-i-1; j++){
+            for (int j = 0; j < i; j++) {
                 System.out.print(" ");
             }
             
             //Star
-            for(int j = 0; j < (i*2)+1; j++){
+            for (int j = 0; j < ((n-i)*2) - 1; j++) {
                 System.out.print("*");
             }
             
             //Space
-            for(int j = 0; j < n-1-i; j++){
+            for (int j = 0; j < i; j++) {
                 System.out.print(" ");
             }
             System.out.print("\n");
         }
     }
-    
+            
 }
