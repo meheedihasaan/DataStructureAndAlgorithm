bc463ab40ca541fb30c9480cb79ee8f425e1dd82
Mehedi Hasan <mehedihasan111941@gmail.com>
2023-03-30 04:21:49 -0700

    Initial Commit

---
+++ src/main/java/pattern/Pattern1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pattern;
+
+/**
+ *
+ * @author User
+ */
+public class Pattern1 {
+    
+    public void printSquare(int n){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+    }
+    
+}
